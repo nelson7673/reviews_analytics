a165da5a9a523ce2ce5e7b1f71ae567b6302f59d
Nelson Hsueh <nelson7673@gmail.com>
2020-05-27 23:13:06 +0800

    add calc average reviews

--- read.py
+++ read.py
@@ -8,8 +8,11 @@ with open('reviews.txt', 'r') as f:
         if count % 1000 == 0:
             print(count)
 
+print('檔案讀取完了, 總共有', len(data), '筆資料！')
 
-print(len(data))
-print(data[0])
-print('----------------------------------')
-print(data[1])
+sum_len = 0
+for d in data:
+    sum_len += len(d)
+
+print('平均是', sum_len / len(data), '長度')
+
